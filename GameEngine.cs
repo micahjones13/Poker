1825054fc45c2aefcab5284682d4b12d4bfde142
micahjones13 <micahjones13@gmail.com>
2020-08-28 08:46:12 -0600

    started DecideWinner func and royalflush func

--- GameEngine.cs
+++ GameEngine.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 class GameEngine
 {
@@ -15,6 +16,7 @@ class GameEngine
         }
         else
         {
+
             return true;
         }
     }
@@ -32,6 +34,34 @@ class GameEngine
             WantToFold = true;
         }
     }
+
+    public void DecideWinner(TexasTable table, Hand playerHand, Hand computerHand)
+    {
+        // needs to use TableList and HandList to determine the hand(straight, flush, ect)
+        // start at highest possible (royal flush), and as soon as one person has it and the other does not, they win
+        // need to compare HandList to TableList see if the cards are there to meet the royal flush reqs
+
+
+        // compare player's hand and computer's hand to see who's is better
+
+
+    }
+    public void RoyalFlush(TexasTable table, Hand hand)
+    {
+        // royal flush - ace(1), king, queen, jack, 10 all of same suit
+        bool isMatching = false;
+        var type = hand.HandList[0].Type;
+        if (hand.HandList[0].Type == hand.HandList[1].Type)
+        {
+            isMatching = true;
+        }
+        else
+        {
+            return;
+        }
+        var filtered = table.TableList.Where(card => card.Type == type); //creates new list matchign the where condition
+
+    }
     public void TexasHoldEm()
     {
         var deck = new Deck();
@@ -42,7 +72,10 @@ class GameEngine
             deck.BuildDeck(); //need to rebuild the deck every round
 
             // Console.WriteLine($"The deck has {deck.DeckList.Count}");
-            Quit();
+            if (Quit())
+            {
+                break;
+            }
             deck.Shuffle();
             deck.Shuffle();
             deck.Shuffle();
